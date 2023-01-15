fbf314ae0ebf7de4078e6d9b47686f31fcb2aeee
Viet Nguyen <viet.nguyen@oivan.com>
2023-01-15 08:56:17 +0700

    update

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ### Packages install
 go get -u package_name
+
+### Logging - NEED TO BE IMPROVED
+fmt.Printf("[ERROR] Failed to connect to MySQL Server")
+LOGLEVEL - CONTENT
--- dbhandlers/mysqlhandler/mysqlhandler.go
+++ dbhandlers/mysqlhandler/mysqlhandler.go
@@ -1,13 +1,24 @@
-package main
+package mysqlhandler
 
 import (
+	"fmt"
+
 	"example.com/m/v2/envhandler"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func main() {
+func conn() *gorm.DB {
 	Host := envhandler.GetEnv("MYSQL_HOST")
 	Port := envhandler.GetEnv("MYSQL_PORT")
 	Username := envhandler.GetEnv("MYSQL_USERNAME")
 	Password := envhandler.GetEnv("MYSQL_PASSWORD")
 	Database := envhandler.GetEnv("MYSQL_DATABASE")
+	Server := Host + ":" + Port
+	dsn := Username + ":" + Password + "@tcp(" + Server + ")/" + Database
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Print("ERROR: ", err)
+	}
+	return db
 }
