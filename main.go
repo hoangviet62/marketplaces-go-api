65f87f39f8b0485166a748464b91a6c75f6676e2
Nguyen Ong <nguyen.ong@oivan.com>
2023-01-24 15:14:32 +0700

    init first route api

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/hoangviet62/marketplaces-go-api/cmd"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/hoangviet62/marketplaces-go-api/cmd"
+	"github.com/hoangviet62/marketplaces-go-api/internal/routes"
+)
 
 func main() {
+	r := gin.Default()
+
 	cmd.InitMySqlConnection()
 	cmd.StartMigration()
 	cmd.StartApiServer()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+
+	routes.CategoryRoutes(r)
+
+	r.Run()
 }
