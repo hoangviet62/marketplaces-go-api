10fee3fb378a61cb9a240a4c9ab6a4725fc23757
Viet Nguyen <viet.nguyen@oivan.com>
2023-01-30 09:45:56 +0700

    permission

--- internal/controllers/product.go
+++ internal/controllers/product.go
@@ -2,10 +2,15 @@ package internal
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
 	// "github.com/hoangviet62/marketplaces-go-api/cmd"
-	model "github.com/hoangviet62/marketplaces-go-api/internal/models"
 	"net/http"
+
+	model "github.com/hoangviet62/marketplaces-go-api/internal/models"
 )
+
+var DB *gorm.DB
 
 func GetProducts(c *gin.Context) {
 	var products []model.Product
