3c275c5e7d72ff9a8f356fc0cb3f2a7ea8ae7a9b
Viet Nguyen <viet.nguyen@oivan.com>
2023-02-04 14:14:55 +0700

    update

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ package main
 import (
 	"github.com/hoangviet62/marketplaces-go-api/cmd"
 	"github.com/hoangviet62/marketplaces-go-api/helpers"
-	kong "github.com/hoangviet62/marketplaces-go-api/internal/services/kong"
 )
 
 func main() {
 	helpers.InitMySqlConnection()
-	kong.CreateConsumer("viet")
 	cmd.StartMigration()
 	cmd.StartApiServer()
 }
