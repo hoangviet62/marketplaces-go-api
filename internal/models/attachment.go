32e3cbf945657c10c59650db80ab5a2aca9730b0
Nguyen Ong <nguyen.ong@oivan.com>
2023-02-06 21:23:28 +0700

    polymorphic for Attachment

--- internal/models/attachment.go
+++ internal/models/attachment.go
@@ -4,11 +4,7 @@ import "gorm.io/gorm"
 
 type Attachment struct {
 	gorm.Model
-	Attachment_type string `json:"attachment_type"`
-	AttachmentID    uint   `json:"attachment_id"`
-	Banner          Banner `gorm:"foreignKey:AttachmentID"`
-	// Category        Category `gorm:"foreignKey:AttachmentID"`
-	// Product         Product  `gorm:"foreignKey:AttachmentID"`
-	// Sku             Sku      `gorm:"foreignKey:AttachmentID"`
-	Url string `json:"url"`
+	AttachmentType string `json:"attachment_type"`
+	AttachmentID   int    `json:"attachment_id"`
+	Url            string `json:"url"`
 }
