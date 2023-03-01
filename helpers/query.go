e66bd657d17b019f98a88de48a1e7fcf27aefee6
nguyen.ong <nguyen.ong@oivan.com>
2023-03-01 15:59:41 +0700

    Update PerPage params

--- helpers/query.go
+++ helpers/query.go
@@ -8,7 +8,7 @@ import (
 func QueryBuilder(queries url.Values) map[string]interface{} {
 	result := make(map[string]interface{})
 	for k, v := range queries {
-		if k != "sort" && k != "page" && k != "search" && k != "perPage" {
+		if k != "sort" && k != "page" && k != "search" && k != "per_page" {
 			result[k] = v
 		}
 	}
