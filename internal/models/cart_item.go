6a79695851cfbc56d0358aa38b7e34300b6e7ed0
nguyen.ong <nguyen.ong@oivan.com>
2023-03-04 22:32:00 +0700

    update

--- internal/models/cart_item.go
+++ internal/models/cart_item.go
@@ -9,3 +9,10 @@ type CartItem struct {
 	Product  Product `json:"product"`
 	Sku      Sku     `json:"sku"`
 }
+
+type CreateCartItemInput struct {
+	Quantity  uint    `json:"quantity"`
+	Price     float64 `json:"price"`
+	CartID    uint    `json:"cart_id"`
+	ProductID uint    `json:"product_id"`
+}
