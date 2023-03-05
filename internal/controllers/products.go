a839b1cb2c2bf6ab24c0441dc81aee9158a42064
nguyen.ong <nguyen.ong@oivan.com>
2023-03-05 11:23:23 +0700

    update

--- internal/controllers/products.go
+++ internal/controllers/products.go
@@ -90,11 +90,63 @@ func GetProductById(context *gin.Context) {
 }
 
 func UpdateProduct(context *gin.Context) {
-	updatedProduct, err := service.UpdateProduct(context, nil)
+	product, err := service.UpdateProduct(context)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
+	}
+
+	attachmentURLs, attachmentURLsErr := service.GetProductAttachments(context)
+
+	if attachmentURLsErr != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": attachmentURLsErr.Error()})
+		return
+	}
+
+	imageURLs := attachmentURLs["images"]
+	mediaURLs := attachmentURLs["medias"]
+
+	var images []model.Attachment
+	var medias []model.Attachment
+
+	for _, imageURL := range imageURLs {
+		attachment, err := attachmentService.CreateAttachment(context, "product_images", product.ID, imageURL)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		images = append(images, attachment)
+	}
+
+	for _, mediaURL := range mediaURLs {
+		attachment, err := attachmentService.CreateAttachment(context, "product_medias", product.ID, mediaURL)
+
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		medias = append(medias, attachment)
+	}
+
+	var updatedProduct model.Product
+
+	if images != nil {
+		updatedProduct, err = service.UploadProductAttachment(product.ID, images, "images")
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	if medias != nil {
+		updatedProduct, err = service.UploadProductAttachment(product.ID, medias, "medias")
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	context.JSON(http.StatusOK, gin.H{"data": updatedProduct})
--- internal/services/products/update_product.go
+++ internal/services/products/update_product.go
@@ -3,13 +3,15 @@ package internal
 import (
 	"errors"
 	// "fmt"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	helpers "github.com/hoangviet62/marketplaces-go-api/helpers"
 	model "github.com/hoangviet62/marketplaces-go-api/internal/models"
-	"strconv"
+	service "github.com/hoangviet62/marketplaces-go-api/internal/services/attachments"
 )
 
-func UpdateProduct(context *gin.Context, attachments []model.Attachment) (model.Product, error) {
+func UpdateProduct(context *gin.Context) (model.Product, error) {
 	product := model.Product{}
 	if err := helpers.DB.Where("id = ?", context.Param("id")).First(&product).Error; err != nil {
 		return product, errors.New("record not found")
@@ -21,12 +23,34 @@ func UpdateProduct(context *gin.Context, attachments []model.Attachment) (model.
 	tag := context.PostForm("tag")
 	categoryId, _ := strconv.ParseUint(context.PostForm("category_id"), 10, 8)
 
+	deleteImages := context.PostFormArray("delete_images")
+	deleteMedias := context.PostFormArray("delete_medias")
+
+	if len(deleteImages) > 0 {
+		for _, image := range deleteImages {
+			imageId, _ := strconv.ParseUint(image, 10, 8)
+			_, error := service.DeleteAttachment(context, uint(imageId))
+			if error != nil {
+				return product, errors.New(error.Error())
+			}
+		}
+	}
+
+	if len(deleteMedias) > 0 {
+		for _, image := range deleteMedias {
+			imageId, _ := strconv.ParseUint(image, 10, 8)
+			_, error := service.DeleteAttachment(context, uint(imageId))
+			if error != nil {
+				return product, errors.New(error.Error())
+			}
+		}
+	}
+
 	input := model.Product{
 		Name:        name,
 		Description: description,
-		CategoryID: uint(categoryId),
-		Tag:        tag,
-		Attachments: attachments,
+		CategoryID:  uint(categoryId),
+		Tag:         tag,
 	}
 
 	if err := helpers.DB.Model(&product).Updates(input).Error; err != nil {
