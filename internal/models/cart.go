72bcf34ca5134eed2994159643727f9f75360f27
nguyen.ong <nguyen.ong@oivan.com>
2023-03-10 13:53:16 +0700

    update

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -3,7 +3,7 @@ package internal
 type Cart struct {
 	Base
 	UserID   uint       `json:"user_id"`
-	CartItem []CartItem `gorm:"constraint:OnDelete:CASCADE;" json:"cart_items"`
+	CartItem []CartItem `gorm:"constraint:OnDelete:SET NULL;" json:"cart_items"`
 }
 
 type CreateCartParams struct {
