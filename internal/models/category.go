168b8a26a5551a8b7c4170117964663437c116b4
nguyen.ong <nguyen.ong@oivan.com>
2023-03-10 21:38:18 +0700

    Update

--- internal/models/category.go
+++ internal/models/category.go
@@ -3,7 +3,7 @@ package internal
 type Category struct {
 	Base
 	Name        string       `json:"name"`
-	Products    []Product    `gorm:"constraint:OnDelete:RESTRICT;" json:"products,omitempty"`
+	Products    []Product    `gorm:"constraint:OnUpdate:RESTRICT;" json:"products,omitempty"`
 	Images      []Attachment `gorm:"polymorphic:Attachment;polymorphicValue:CategoryImages" json:"images,omitempty"`
 	Medias      []Attachment `gorm:"polymorphic:Attachment;polymorphicValue:CategoryMedias" json:"medias,omitempty"`
 	Attachments []Attachment `gorm:"polymorphic:Attachment;" json:"attachments,omitempty"`
--- internal/services/categories/delete_category.go
+++ internal/services/categories/delete_category.go
@@ -15,7 +15,7 @@ func DeleteCategory(context *gin.Context) (bool, error) {
 		return false, errors.New(err.Error())
 	}
 
-	if err := DB.Select("Images", "Medias", "Attachments").Unscoped().Delete(&category).Error; err != nil {
+	if err := DB.Select("Images", "Medias", "Attachments", "Products").Delete(&category).Error; err != nil {
 		return false, errors.New(err.Error())
 	}
 
