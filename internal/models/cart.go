e5aa06ba44cc98cf8a277b5edd99f7e9a95bac3b
nguyen.ong <nguyen.ong@oivan.com>
2023-03-13 14:15:52 +0700

    update

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -1,5 +1,10 @@
 package internal
 
+import (
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
 type Cart struct {
 	Base
 	UserID   uint       `json:"user_id"`
@@ -9,4 +14,9 @@ type Cart struct {
 type CreateCartParams struct {
 	Product Product
 	Cart    Cart
+}
+
+func (cart *Cart) AfterCreate(tx *gorm.DB) (err error) {
+	tx.Preload(clause.Associations).Preload("CartItem.Product.Images").Preload("CartItem.Product.Medias").First(&cart, "id = ?", cart.ID)
+	return
 }
